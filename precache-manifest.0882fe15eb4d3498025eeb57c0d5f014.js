be98e672664e92a486657e107548158b35035164
neiljaitly7963 <neiljaitly7963@gmail.com>
2020-08-27 03:39:21 +0530

    Updates

---
+++ precache-manifest.0882fe15eb4d3498025eeb57c0d5f014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28ef56b82fb277556b0e3190d9c0294",
+    "url": "/kommunicate/index.html"
+  },
+  {
+    "revision": "6ea1d501b1634ded44a8",
+    "url": "/kommunicate/static/css/main.a2c1c2f3.chunk.css"
+  },
+  {
+    "revision": "13fc4dfc0064cb9aee6f",
+    "url": "/kommunicate/static/js/2.65459871.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kommunicate/static/js/2.65459871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ea1d501b1634ded44a8",
+    "url": "/kommunicate/static/js/main.87075c2f.chunk.js"
+  },
+  {
+    "revision": "120e9c2e95a3a5a37272",
+    "url": "/kommunicate/static/js/runtime-main.c2f6efa8.js"
+  }
+]);
